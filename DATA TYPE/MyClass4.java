5b6b5156fa58c1c022cb382242675018d3402afe
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-09-07 12:48:02 +0600

    Add files via upload

---
+++ DATA TYPE/MyClass4.java
@@ -0,0 +1,19 @@
+class MyClass4 { 
+    public static void main(String args[]) 
+    { 
+        byte a = 126; 
+  
+    
+        System.out.println(a); 
+  
+        a++; 
+        System.out.println(a); 
+  
+        a++; 
+        System.out.println(a); 
+  
+      
+        a++; 
+        System.out.println(a); 
+    } 
+} 
