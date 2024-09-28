3ae7822a273a3888ecc1bead6ddd307c51e4be66
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-09-29 01:30:17 +0600

    TryCatch

--- Exception Handling/finally/try catch/TryCatchExample4.java
+++ Exception Handling/finally/try catch/TryCatchExample4.java
@@ -3,9 +3,9 @@ public class TryCatchExample4 {
     public static void main(String[] args) {  
         try  
         {  
-        int data=50/0; //may throw exception   
+        int data=50/0; 
         }  
-            // handling the exception by using Exception class      
+   
         catch(Exception e)  
         {  
             System.out.println(e);  
