fcf304cf6c9f8e563ddeab11b3c087b860b5b0b2
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-09-29 01:29:30 +0600

    Update TryCatchExample2.java

--- Exception Handling/finally/try catch/TryCatchExample2.java
+++ Exception Handling/finally/try catch/TryCatchExample2.java
@@ -3,9 +3,9 @@ public class TryCatchExample2 {
     public static void main(String[] args) {  
         try  
         {  
-        int data=50/0; //may throw exception   
+        int data=50/0;  
         }  
-            //handling the exception  
+         
         catch(ArithmeticException e)  
         {  
             System.out.println(e);  
