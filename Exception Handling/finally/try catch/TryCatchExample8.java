b91af4f435d44c69a5ada046561ccb9ab1edd027
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-10-02 05:26:21 +0600

    TryCatch

--- Exception Handling/finally/try catch/TryCatchExample8.java
+++ Exception Handling/finally/try catch/TryCatchExample8.java
@@ -3,10 +3,10 @@ public class TryCatchExample8 {
     public static void main(String[] args) {  
         try  
         {  
-        int data=50/0; //may throw exception   
+        int data=50/0; 
   
         }  
-            // try to handle the ArithmeticException using ArrayIndexOutOfBoundsException  
+    
         catch(ArrayIndexOutOfBoundsException e)  
         {  
             System.out.println(e);  
