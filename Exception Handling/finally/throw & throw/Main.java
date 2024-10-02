6e8de552442559a6e90d748e1c11b9cb7b826663
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-10-03 05:09:49 +0600

    Add files via upload

---
+++ Exception Handling/finally/throw & throw/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Main {
+  public static void findFile() throws IOException {
+    throw new IOException("File not found");
+  }
+
+  public static void main(String[] args) {
+    try {
+      findFile();
+      System.out.println("Rest of code in try block");
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+}
