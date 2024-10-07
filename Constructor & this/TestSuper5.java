90b147bb481f5f171cef26c34ec23277d095de74
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-10-07 20:57:30 +0600

    Update TestSuper5

--- Constructor & this/TestSuper5.java
+++ Constructor & this/TestSuper5.java
@@ -9,7 +9,7 @@ this.name=name;
 class Emp extends Person{  
 float salary;  
 Emp(int id,String name,float salary){  
-super(id,name);//reusing parent constructor  
+super(id,name);
 this.salary=salary;  
 }  
 void display(){System.out.println(id+" "+name+" "+salary);}  
