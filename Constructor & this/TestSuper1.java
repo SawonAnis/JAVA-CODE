d23f91656b9ba05dba0bcc55d0b530021367bf43
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-10-07 20:57:59 +0600

    Update TestSuper1

--- Constructor & this/TestSuper1.java
+++ Constructor & this/TestSuper1.java
@@ -4,8 +4,8 @@ String color="white";
 class Dog extends Animal{  
 String color="black";  
 void printColor(){  
-System.out.println(color);//prints color of Dog class  
-System.out.println(super.color);//prints color of Animal class  
+System.out.println(color);
+System.out.println(super.color);
 }  
 }  
 class TestSuper1{  
