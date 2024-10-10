e36cf38e38d9aafc95defeb7af1e436dd70f603e
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-10-11 01:27:39 +0600

    Update ReverseArray

--- Array/ReverseArray.java
+++ Array/ReverseArray.java
@@ -1,6 +1,6 @@
 public class ReverseArray {  
     public static void main(String[] args) {  
-        //Initialize array  
+ 
         int [] arr = new int [] {1, 2, 3, 4, 5};  
         System.out.println("Original array: ");  
         for (int i = 0; i < arr.length; i++) {  
@@ -8,7 +8,7 @@ public class ReverseArray {
         }  
         System.out.println();  
         System.out.println("Array in reverse order: ");  
-        //Loop through the array in reverse order  
+
         for (int i = arr.length-1; i >= 0; i--) {  
             System.out.print(arr[i] + " ");  
         }  
