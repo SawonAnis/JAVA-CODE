a4443af281bac9f2586ae3cdf92e7df3cb4187d5
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-10-16 05:47:02 +0600

    Add files via upload

---
+++ Abstraction/Demo1.java
@@ -0,0 +1,20 @@
+abstract class MyClass{
+   public void disp(){
+     System.out.println("Concrete method of parent class");
+   }
+   abstract public void disp2();
+}
+
+class Demo1 extends MyClass{
+   /* Must Override this method while extending
+    * MyClas
+    */
+   public void disp2()
+   {
+       System.out.println("overriding abstract method");
+   }
+   public static void main(String args[]){
+       Demo1 obj = new Demo1();
+       obj.disp2();
+   }
+}
