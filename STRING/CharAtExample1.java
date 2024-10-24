3cf11197304db9f2ac1fc39f00d4e391f4769c86
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-10-24 16:49:28 +0600

    CharAtExample

--- STRING/CharAtExample1.java
+++ STRING/CharAtExample1.java
@@ -1,6 +1,6 @@
 public class CharAtExample1{  
 public static void main(String args[]){  
 String name="JAVA";  
-char ch=name.charAt(10);//returns the char value at the 10th index  
+char ch=name.charAt(10);
 System.out.println(ch);  
 }}  
