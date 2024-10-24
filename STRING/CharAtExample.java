b32f1b9897e03fbcbe6013ce246ab1683b1f5880
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-10-24 16:47:33 +0600

    Update CharAtExample.java

--- STRING/CharAtExample.java
+++ STRING/CharAtExample.java
@@ -1,6 +1,6 @@
 public class CharAtExample{  
 public static void main(String args[]){  
 String name="Java";  
-char ch=name.charAt(2);//returns the char value at the 4th index  
+char ch=name.charAt(2);
 System.out.println(ch);  
 }}
