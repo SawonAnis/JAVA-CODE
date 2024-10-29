d01e806891bcb71dc38bb4d06cabc700792632b5
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-10-29 15:44:10 +0600

    Update StringInJava.java

--- STRING/StringInJava.java
+++ STRING/StringInJava.java
@@ -1,9 +1,7 @@
 public class StringInJava {
     public static void main(String[] args) {
 
-//        char[] name = {'N', 'A', 'Y', 'E', 'M'};
-//        String name3 = new String(name);
-//        String name2 = name3;
+
 
         String str = "Java is Awesome";
         String str3 = "Java is Awesome";
