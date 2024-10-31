84249395a293868617ac3110740a3511b755dc6d
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-11-01 02:19:52 +0600

    Add files via upload

---
+++ DATA TYPE/StudentData.java
@@ -0,0 +1,16 @@
+class StudentData{  
+
+   final int ROLL_NO;
+	 
+   StudentData(int rnum){
+      
+      ROLL_NO=rnum;
+   }
+   void myMethod(){  
+      System.out.println("Roll no is:"+ROLL_NO);
+   }  
+   public static void main(String args[]){  
+      StudentData obj=new  StudentData(1234);  
+      obj.myMethod();  
+   }  
+}
