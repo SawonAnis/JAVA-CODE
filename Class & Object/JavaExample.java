4e3a52113d594bc5a8e9bb8a667b7d1575d1280e
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-11-02 01:41:57 +0600

    Update JavaExample.java

--- Class & Object/JavaExample.java
+++ Class & Object/JavaExample.java
@@ -1,16 +1,11 @@
 class JavaExample{
    private static String str = "BeginnersBook";
 
-   //Static class
+
    static class MyNestedClass{
-	//non-static method
+
 	public void disp() {
 
-	   /* If you make the str variable of outer class
-	    * non-static then you will get compilation error
-	    * because: a nested static class cannot access non-
-	    * static members of the outer class.
-	    */
 	   System.out.println(str); 
 	}
 
