e28c7073337a0c6ae14827f7c815d28a2b59033a
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-11-12 23:22:00 +0600

    JavaExample

--- static/JavaExample.java
+++ static/JavaExample.java
@@ -1,7 +1,7 @@
 class JavaExample{
    static int i = 10;
    static String s = "Beginnersbook";
-   //This is a static method
+
    public static void main(String args[]) 
    {
        System.out.println("i:"+i);
