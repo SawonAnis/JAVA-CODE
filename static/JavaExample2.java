e6c87075e392c60b95fa209755219d85cdb1dd61
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-11-30 03:26:48 +0600

    JavaExample2

--- static/JavaExample2.java
+++ static/JavaExample2.java
@@ -1,13 +1,13 @@
 class JavaExample2{
    static int num;
    static String mystr;
-   //First Static block
+
    static{
       System.out.println("Static Block 1");
       num = 68;
       mystr = "Block1";
   } 
-  //Second static block
+
   static{
       System.out.println("Static Block 2");
       num = 98;
