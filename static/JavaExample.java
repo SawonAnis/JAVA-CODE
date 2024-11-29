22e03ce2d48a5504a4b08a7ff3baa09fe09404f3
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-11-30 03:25:26 +0600

    Add files via upload

--- static/JavaExample.java
+++ static/JavaExample.java
@@ -1,10 +1,13 @@
 class JavaExample{
-   static int i = 10;
-   static String s = "Beginnersbook";
-
-   public static void main(String args[]) 
+   static int num;
+   static String mystr;
+   static{
+      num = 97;
+      mystr = "Static keyword in Java";
+   }
+   public static void main(String args[])
    {
-       System.out.println("i:"+i);
-       System.out.println("s:"+s);
+      System.out.println("Value of num: "+num);
+      System.out.println("Value of mystr: "+mystr);
    }
 }
