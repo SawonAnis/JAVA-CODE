8407490b656998aef17a6cbce38be380a3f834bb
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-12-03 04:09:31 +0600

    Update Counter.java

--- static/Counter.java
+++ static/Counter.java
@@ -1,15 +1,14 @@
-//Java Program to demonstrate the use of an instance variable  
-//which get memory each time when we create an object of the class.  
+
 class Counter{  
-int count=0;//will get memory each time when the instance is created  
+int count=0;
   
 Counter(){  
-count++;//incrementing value  
+count++;  
 System.out.println(count);  
 }  
   
 public static void main(String args[]){  
-//Creating objects  
+
 Counter c1=new Counter();  
 Counter c2=new Counter();  
 Counter c3=new Counter();  
